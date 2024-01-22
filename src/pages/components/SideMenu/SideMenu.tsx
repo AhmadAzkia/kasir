22629d38372860566bf5e1fe40fee713b17f1afd
DifaaNug <123245678+DifaaNug@users.noreply.github.com>
2024-01-22 08:03:54 +0700

    Add Motion and Remixicon

--- src/pages/components/SideMenu/SideMenu.tsx
+++ src/pages/components/SideMenu/SideMenu.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 const SideMenu = () => {
     return (
-       <ul>
-        <li>Setting</li>
-        <li>Profile</li>
-       </ul>
+       <div className='fixed left-0 top-0 w-64 h-full bg-gradient-to-br from-blue-600 to-cyan-300 p-4'>
+        <div className="flex items-center pb-4 border-b border-white">
+            <img className='w-10 h-10 rounded object-cover' src="https://placehold.co/32x32" alt="" />
+            <span className='text-lg font-bold text-white ml-3'>Kasir</span>
+        </div>
+       </div>
     )
 }
 
